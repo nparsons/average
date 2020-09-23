5676f53a0288ab1defb1d5988842da0c8fb28d90
Nicholas Parsons <nparsons@localhost.localdomain>
2020-09-23 14:04:49 -0400

    Updated program to read the data from a file instead of using hard-coded values.

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 // average calculates the average of several numbers in a set.
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "log"
+  "github.com/nparsons/datafile"
+)
 
 func main() {
-  numbers := [3]float64{71.8, 56.2, 89.5}
+  numbers, err := datafile.GetFloats("data.txt")
+  if err != nil {
+    log.Fatal(err)
+  }
+
   var sum float64 = 0
   for _, number := range numbers {
     sum += number
